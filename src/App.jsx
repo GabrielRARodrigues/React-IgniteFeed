867e023dd3e055dbf674cd94f67596ab686138b7
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-07-30 16:55:15 -0300

    start: Starting the construction of the 'Ignite Feed' project using ReactJs. Using 'vite' to create the project structure, removing some unnecessary files, and creating the draft of the first component of the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from './components/Post'
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Gabriel Rodrigues"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima non itaque beatae? Ullam sapiente nesciunt nam inventore eius explicabo ipsa unde illum impedit possimus quidem vitae sequi voluptatibus, iste rerum."
+      />
+      <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+    </div>
+  )
+}

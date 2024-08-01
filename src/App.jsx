eb571409f0ee4fa17675b85705aace7d35762c6d
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-08-01 11:17:07 -0300

    raw: Starting the construction of the main content of the page. Creating styles for the 'App' component and refactoring the component to prepare the structure for adding the main content. Finally, creating the 'Sidebar' component and using it within the 'App' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { Header } from './components/Header'
-import { Post } from './components/Post'
-
 import './global.css'
+
+import styles from './App.module.css'
+
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Gabriel Rodrigues"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima non itaque beatae? Ullam sapiente nesciunt nam inventore eius explicabo ipsa unde illum impedit possimus quidem vitae sequi voluptatibus, iste rerum."
-      />
-      <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Gabriel Rodrigues"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima non itaque beatae? Ullam sapiente nesciunt nam inventore eius explicabo ipsa unde illum impedit possimus quidem vitae sequi voluptatibus, iste rerum."
+          />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+        </main>
+      </div>
     </div>
   )
 }

17f532818675d7085bee3881ea495d495c8d0c79
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-08-05 09:29:17 -0300

    raw: Creating the 'Comment' component, which represents a comment on a post, assembling the HTML structure of the component, and applying the necessary styles. Finalizing the construction of the 'Post' component by creating the comment form, displaying the post's comments, and applying the final styles

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/GabrielRARodrigues.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Rodrigues</strong>
+              <time
+                title="1 de Agosto às 10:30h"
+                dateTime="2024-08-01 10:30:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,7 @@
 import styles from './Post.module.css'
 
+import { Comment } from './Comment'
+
 export function Post() {
   return (
     <article className={styles.post}>
@@ -35,6 +37,20 @@ export function Post() {
           <a href="">#nlw </a>
           <a href="">#rocketseat</a>
         </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário"></textarea>
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     </article>
   )
